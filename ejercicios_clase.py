1f46a1abec3b32ad546cc68963fb3d05bbca6a57
MartinVillodas <58822330+MartinVillodas@users.noreply.github.com>
2020-07-26 19:50:49 -0300

    Update ejercicios_clase.py

--- ejercicios_clase.py
+++ ejercicios_clase.py
@@ -35,13 +35,13 @@ def ej1():
     # El resultado de sumar 4 y 2 es 6
 
     # Suma
-
+    print("El resultado de sumar", numero_1,"y", numero_2, "es", numero_1 + numero_2)
     # Resta
-
+    print("El resultado de restar", numero_1,"y", numero_2, "es", numero_1 - numero_2)
     # División
-
+    print("El resultado de dividir", numero_1,"y", numero_2, "es", numero_1 / numero_2)
     # Multiplicación
-
+    print("El resultado de multiplicar", numero_1,"y", numero_2, "es", numero_1 * numero_2)
 
 def ej2():
     # Ejercicios de práctica numérica
@@ -62,12 +62,14 @@ def ej2():
     # El resultado de sumar 4 y 2 es 6
 
     # Suma
-
+    print("El resultado de sumar", numero_3,"y", numero_4, "es", numero_3 + numero_4)
     # Resta
-
+    print("El resultado de restar", numero_3,"y", numero_4, "es", numero_3 - numero_4)
     # División
-
+    print("El resultado de dividir", numero_3,"y", numero_4, "es", numero_3 / numero_4)
     # Multiplicación
+    print("El resultado de multiplicar", numero_3,"y", numero_4, "es", numero_3 * numero_4)
+
 
 
 def ej3():
@@ -82,14 +84,14 @@ def ej3():
     apellido = str(input())
 
     # Imprima su nombre completo
-
+    print(nombre,apellido)
     # Almacenar su nombre completo en una variable
     # nombre_completo = .....
-
+    nombre_completo = nombre + "" + apellido
     # Imprimir la cantidad de letras que posee su nombre completo
+    print(len(nombre_completo))
 
-
-def ej3():
+def ej4():
     # Ejemplos variables de texto
 
     # Ingrese tres palabras y arme un acrónimo con ellas
@@ -106,9 +108,9 @@ def ej3():
     # De cada palabra debe tomar la primera letra y armar el acrónimo
     # Ejemplo: Alumbrado, barrido y limpieza --> ABL
     # Imprimir el resultado en pantalla
+    print(palabra_1[0],palabra_2[0],palabra_3[0])
 
-
-def ej4():
+def ej5():
     # Ejemplos variables de texto
 
     # Ingrese dos palabras y arme combinaciones con ella
@@ -122,10 +124,12 @@ def ej4():
     # De la segunda palabra tome las últimas tres letras, utilice el operador :
     # Formar una nueva palabra con los recortes solicitados
     # Imprima en pantalla los resultados
+    print(palabra_1[0:4] + palabra_2[-1:])
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
     ej1()
-    # ej2()
-    # ej3()
-    # ej4()
+    ej2()
+    ej3()
+    ej4()
+    ej5()
